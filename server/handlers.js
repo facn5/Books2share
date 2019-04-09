84f7f31c45f9b4f2c7c2e9cbe53001b52ab25ea1
mrfong <therealshadix@gmail.com>
2019-04-09 12:52:10 +0300

    defined extension types

--- server/handlers.js
+++ server/handlers.js
@@ -1,2 +1,20 @@
 const fs = require("fs");
 const path = require("path");
+
+var exType = {
+  html: {
+    "Content-Type": "text/html"
+  },
+  css: {
+    "Content-Type": "text/css"
+  },
+  js: {
+    "Content-Type": "application/javascript"
+  },
+  json: {
+    "Content-Type": "application/json"
+  },
+  ico: {
+    "Content-Type": "image/vnd.microsoft.icon"
+  }
+};
