7d10f2d717b52d675e30b56e2947a2a1e6f3d335
mrfong <therealshadix@gmail.com>
2019-04-09 12:53:04 +0300

    added an index handler

--- server/handlers.js
+++ server/handlers.js
@@ -18,3 +18,17 @@ var exType = {
     "Content-Type": "image/vnd.microsoft.icon"
   }
 };
+
+const indexHandler = res => {
+  var filePath = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(filePath, function(error, file) {
+    if (error) {
+      res.writeHead(500, exType.html);
+      res.end("<h1>sorry, something went wrong</h1>");
+    } else {
+      res.writeHead(200, exType.html);
+      res.write(file);
+    }
+    res.end();
+  });
+};
