2b4858680d7bc47eeda40c2eb56e8940415b6088
mrfong <therealshadix@gmail.com>
2019-04-10 12:38:30 +0300

    fixed more designs

--- server/router.js
+++ server/router.js
@@ -3,16 +3,11 @@ const router = (req, res) => {
   var url = req.url;
   if (url === "/") {
     handler.index(res);
-<<<<<<< HEAD
   } else if (
     url.includes("public") ||
     url.includes("search.js") ||
     url.includes("style.css")
   ) {
-=======
-    //    handler.Datahan(res);
-  } else if (url.includes("public") || url.includes("search.js")) {
->>>>>>> acacb8cb49daa420cd6567471c6a3c35937c6df3
     handler.assets(url, res);
   } else if (url.includes("search?")) {
     handler.search(url, res);
